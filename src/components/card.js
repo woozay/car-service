8ee83d88dec946f493d5bb3237207cdcaa141d16
Nabeel Amir <Nabeel.Amir@alcumusgroup.com>
2019-04-30 11:58:18 +0100

    added title to columns

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,19 @@
 import React from "react"
 
-export default ( { title, content } ) => (
+export default ({ title, content }) => (
     <div class="column is-4">
-    <div class="card is-shady">
-        <div class="card-image">
-            <figure class="image is-4by3">
-                <img src="https://i.imgsafe.org/3f/3f0c578f9d.jpeg" alt="Placeholder image" />
-            </figure>
-        </div>
-        <div class="card-content">
-            <div class="content">
-                <h4>{title}</h4>
-                <p>{content}</p>
-                <span class="button is-link modal-button">Modal card</span>
+        <div class="card is-shady">
+            <div class="card-image">
+                <figure class="image is-4by3">
+                    <img src="https://i.imgsafe.org/3f/3f0c578f9d.jpeg" alt="Placeholder image" />
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="content">
+                    <h4>{title}</h4>
+                    <p>{content}</p>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Card from "../components/card"
 
 export default () => (
     <Layout>
-        <section className="hero is-info is-medium">
+        <section className="hero is-info is-large">
             <div className="hero-body">
                 <div className="container">
                     <h1 className="title">Large title</h1>
@@ -12,11 +12,21 @@ export default () => (
                 </div>
             </div>
         </section>
-        <section className="container">
-            <div class="columns features">
-                <Card title="EVERY JOB IS PERSONAL" content="If you want the quality you would expect from the dealership, but with a more personal and friendly atmosphere, you have found it."/>
-                <Card title="BEST MATERIALS" content="We have invested in all the latest specialist tools and diagnostic software that is specifically tailored for the software in your vehicle."/>
-                <Card title="PROFESSIONAL STANDARDS" content="Our auto repair shop is capable of servicing a variety of models. We only do the work that is needed to fix your problem."/>
+        <section className="section">
+            <div className="container has-text-centered">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 className="title">WHY CHOOSE US?</h1>
+                        <h2 className="subtitle">We are one of the leading auto repair shops serving customers in Tucson. All mechanic services are performed by highly qualified mechanics.</h2>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div class="columns is-centered">
+                    <Card title="EVERY JOB IS PERSONAL" content="If you want the quality you would expect from the dealership, but with a more personal and friendly atmosphere, you have found it." />
+                    <Card title="BEST MATERIALS" content="We have invested in all the latest specialist tools and diagnostic software that is specifically tailored for the software in your vehicle." />
+                    <Card title="PROFESSIONAL STANDARDS" content="Our auto repair shop is capable of servicing a variety of models. We only do the work that is needed to fix your problem." />
+                </div>
             </div>
         </section>
     </Layout>
