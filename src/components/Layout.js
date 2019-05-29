41a20f751a1dfa4ccd58c5cce5943bacb0b020f8
Nabeel Amir <Nabeel.Amir@alcumusgroup.com>
2019-05-29 15:10:42 +0100

    renamed footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import './style.scss';
 import Navbar from "./Navbar";
-import Footer from "./footer";
+import Footer from "./Footer";
 
 export default ({ children }) => (
   <div>    
